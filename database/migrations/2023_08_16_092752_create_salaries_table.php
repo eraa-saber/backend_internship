7dc4334435bcc6f980d7db4ed615278b31e4ab87
MarwaAwd <marwaawd5@gmail.com>
2023-08-16 13:03:33 +0300

    addingServicesAndSalariesTable

---
+++ database/migrations/2023_08_16_092752_create_salaries_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('salaries', function (Blueprint $table) {
+            $table->string('id');
+            $table->string('companyCommercialRegister');
+            $table->boolean('isRecordedAddedValue');
+            $table->boolean('isTaxCompliant');
+            $table->string('terrorismFunding');
+            $table->string('paymentFunding');
+            $table->double('salary',20,8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('salaries');
+    }
+};
